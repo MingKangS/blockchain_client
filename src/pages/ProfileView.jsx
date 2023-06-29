3a104b7511025887bcebca5a7d7cfcf9c36c65fc
Siow Ming Kang <62229606+MingKangS@users.noreply.github.com>
2023-06-29 15:24:30 +0000

    loading spinner

--- src/pages/ProfileView.jsx
+++ src/pages/ProfileView.jsx
@@ -3,8 +3,10 @@ import { SmartContractContext } from "../context/SmartContractContext";
 import "../styles/Profile.scss";
 import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { FaPen, FaImage } from "react-icons/fa";
+import LoadingSpinner from "../components/loadingSpinner";
 
 const ProfileView = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [profilePhoto, setProfilePhoto] = useState(
     "https://cdn-icons-png.flaticon.com/512/6522/6522516.png"
   );
@@ -16,37 +18,38 @@ const ProfileView = () => {
   };
 
   const onNewImageInput = (e) => {
-    console.log(e.target.files[0])
-    setProfilePhoto(URL.createObjectURL(e.target.files[0]))
+    console.log(e.target.files[0]);
+    setProfilePhoto(URL.createObjectURL(e.target.files[0]));
   };
-  
+
   return (
     <div className="container">
-      <div className="profile-box">
-        <div className="photo-container">
-          <img
-            className="profile-photo"
-            src={profilePhoto}
-          />
-          <div
-            className="edit-photo"
-            onClick={() => imageInput.current.click()}
-          >
-            <FaImage style={{ margin: "auto" }} />
-            <input
-              ref={imageInput}
-              type="file"
-              onChange={onNewImageInput}
-              style={{ display: "none" }}
-            />
+      {isLoading ? (
+        <LoadingSpinner size={80}/>
+      ) : (
+        <div className="profile-box">
+          <div className="photo-container">
+            <img className="profile-photo" src={profilePhoto} />
+            <div
+              className="edit-photo"
+              onClick={() => imageInput.current.click()}
+            >
+              <FaImage style={{ margin: "auto" }} />
+              <input
+                ref={imageInput}
+                type="file"
+                onChange={onNewImageInput}
+                style={{ display: "none" }}
+              />
+            </div>
           </div>
-        </div>
 
-        <h1 className="username">
-          @someName&nbsp;
-          <FaPen style={{ fontSize: "18px" }} onClick={editUsername} />
-        </h1>
-      </div>
+          <h1 className="username">
+            @someName&nbsp;
+            <FaPen style={{ fontSize: "18px" }} onClick={editUsername} />
+          </h1>
+        </div>
+      )}
     </div>
   );
 };

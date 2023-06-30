3f739b58b7c80a1878c570cc999da03e99d5acb6
Siow Ming Kang <62229606+MingKangS@users.noreply.github.com>
2023-06-30 14:42:44 +0000

    add comments on post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,19 +2,75 @@ import moment from "moment/moment";
 import "../styles/components/Post.scss";
 import draftToHtml from "draftjs-to-html";
 import sanitizeHtml from "sanitize-html";
+import { BiUpvote, BiDownvote } from "react-icons/bi";
+import { shadeColor } from "../utils/functions";
 
 const Post = ({ post }) => {
+  const lightenColor = (color) => {
+    return (
+      "#" +
+      color
+        .replace(/^#/, "")
+        .replace(/../g, (color) =>
+          (
+            "0" +
+            Math.min(255, Math.max(0, parseInt(color, 16) + 180)).toString(16)
+          ).substr(-2)
+        )
+    );
+  };
   return (
     <div className="post-container">
       <div className="post-header">
-        <p className="author">{post.author}</p>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png"
+          className="author-photo"
+        />
+        <p className="author" style={{ color: post.author.color }}>
+          {post.author.username}
+        </p>
         <p className="timestamp">{moment.unix(post.timestamp).fromNow()}</p>
       </div>
       <p
         className="postContent"
         dangerouslySetInnerHTML={{
           __html: sanitizeHtml(draftToHtml(JSON.parse(post.postContent))),
-        }}></p>
+        }}
+      ></p>
+      <div className="interactions">
+        <div className="comments-count">{post.comments.length} comments</div>
+        <div className="votes">
+          <div className="vote-number">
+            {post.upvotes} <BiUpvote className="vote-icon" />
+          </div>
+          <div className="vote-number">
+            {post.downvotes} <BiDownvote className="vote-icon" />
+          </div>
+        </div>
+      </div>
+      {post.comments.map((comment) => (
+        <div style={{ backgroundColor: "#F4F4F4", padding: 6, marginTop: 8 }}>
+          <div className="comment-header">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/6522/6522516.png"
+              className="comment-photo"
+            />
+            <p
+              className="comment-username"
+              style={{ color: comment.author.color }}
+            >
+              {comment.author.username}
+            </p>
+            <p className="comment-timestamp">
+              {moment.unix(comment.timestamp).fromNow()}
+            </p>
+          </div>
+          <p className="comment-text">{comment.text}</p>
+        </div>
+      ))}
+      <div>
+        
+      </div>
     </div>
   );
 };
--- src/context/SmartContractContext.jsx
+++ src/context/SmartContractContext.jsx
@@ -57,7 +57,7 @@ export const SmartContractProvider = ({ children }) => {
       alert("Please connect your MetaMask account and refresh the page.");
     }
   };
-  
+
   const getProfile = async () => {
     try {
       if (ethereum) {
@@ -75,12 +75,14 @@ export const SmartContractProvider = ({ children }) => {
 
         return structuredPosts;
       } else {
-        console.log("Ethereum is not present. Your browser does not have support for the ethereum blockchain network.");
+        console.log(
+          "Ethereum is not present. Your browser does not have support for the ethereum blockchain network."
+        );
       }
     } catch (error) {
       console.log(error);
     }
-    return []
+    return [];
   };
 
   const getAllPosts = async () => {
@@ -100,12 +102,36 @@ export const SmartContractProvider = ({ children }) => {
 
         return structuredPosts;
       } else {
-        console.log("Ethereum is not present. Your browser does not have support for the ethereum blockchain network.");
+        console.log(
+          "Ethereum is not present. Your browser does not have support for the ethereum blockchain network."
+        );
       }
     } catch (error) {
       console.log(error);
     }
-    return []
+    return [
+      {
+        author: { username: "post.author", profilePhoto: "", color: "#123456" },
+        postContent:
+          '{"entityMap":{"0":{"type":"IMAGE","mutability":"MUTABLE","data":{"height":"112","src":"https://raw.githubusercontent.com/facebook/draft-js/master/examples/draft-0-10-0/convertFromHTML/image.png","width":"200"}}},"blocks":[{"key":"64v27","text":"Bold text idk what is sthie eids tai asdsospeo sekj e eijea e aaa igna aindgeingfkdmnegk sdgksd skdgsd skdgjsodi gsgs. sd sd s .sgsgsnesonkenlsefsd.fsfsdg.swegseshn esoeia emfekie aeij sm tshttps://raw.githubusercontent.com/facebook/draft-js/master/examples/draft-0-10-0/convertFromHTML/image.png","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":0,"length":9,"style":"BOLD"}],"entityRanges":[{"offset":9,"length":106,"key":0}],"data":{}}]}',
+        timestamp: 92344567,
+        upvotes: 12,
+        downvotes: 4,
+        comments: [{
+          author: { username: "dijkstra", profilePhoto: "", color: "#FF3456" },
+          text: "lmao this shyt is so tru lmairfjew reia eirjasera .aerawirawrijw wirjwoaw a. aewra ew.",
+          timestamp: 996544573
+        },{
+          author: { username: "dijkstrlmaoa", profilePhoto: "", color: "#6F34EE" },
+          text: "lmaoasg adfg",
+          timestamp: 783645735
+        },{
+          author: { username: "dud", profilePhoto: "", color: "#3FCVAE" },
+          text: "lmaoasg adfg",
+          timestamp: 783645735
+        }]
+      },
+    ];
   };
 
   const addNewPost = async (postContent, timestamp) => {
@@ -135,7 +161,8 @@ export const SmartContractProvider = ({ children }) => {
         getAllPosts,
         addNewPost,
         checkIfWalletIsConnected,
-      }}>
+      }}
+    >
       {children}
     </SmartContractContext.Provider>
   );

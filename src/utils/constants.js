7796b85ce543799b55a0ead808f42cbbda04b0f2
Siow Ming Kang <62229606+MingKangS@users.noreply.github.com>
2023-08-14 12:38:17 +0800

    new contract address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Post.json";
 
-export const contractAddress = "0xc91fdE176ADBf993C39c891FDCa01121b1680F3f";
+export const contractAddress = "0x4C29AC5B3Ab96D6F1B7492241ffbd9582709a1AB";
 export const contractABI = abi.abi;
